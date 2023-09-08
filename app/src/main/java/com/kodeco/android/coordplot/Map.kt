107769de2e506cef29043733167fb2d8849318aa
Andrii Zubenko <andrei.zubenko@gmail.com>
2023-09-07 17:08:33 -0700

    add cursor icon and update Map composable

--- app/src/main/java/com/kodeco/android/coordplot/Map.kt
+++ app/src/main/java/com/kodeco/android/coordplot/Map.kt
@@ -1,14 +1,15 @@
 package com.kodeco.android.coordplot
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
@@ -18,17 +19,17 @@ fun Map(xPercent: Float, yPercent: Float) {
     Box(
         modifier = Modifier
             .size(300.dp)
-            .background(Color.Blue)
+            .background(Color.LightGray)
     ) {
-        Box(
+        Image(
+            painter = painterResource(id = R.drawable.cursor_icon),
+            contentDescription = stringResource(R.string.cursor_icon_desc),
             modifier = Modifier
                 .size(36.dp)
                 .offset(
                     x = (xPercent * 300 - 18).dp,
                     y = (yPercent * 300 - 18).dp
                 )
-                .clip(shape = CircleShape)
-                .background(Color.Green)
         )
     }
 }
