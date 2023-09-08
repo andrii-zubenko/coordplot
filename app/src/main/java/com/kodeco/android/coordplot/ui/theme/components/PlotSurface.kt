aec69e5b272f2c1a99034e8f189d1a89ab257cd7
Andrii Zubenko <andrei.zubenko@gmail.com>
2023-09-08 14:18:04 -0700

    create seperate layots for both landscape and portrait screen orientation

--- app/src/main/java/com/kodeco/android/coordplot/ui/theme/components/PlotSurface.kt
+++ app/src/main/java/com/kodeco/android/coordplot/ui/theme/components/PlotSurface.kt
@@ -1,7 +1,9 @@
 package com.kodeco.android.coordplot.ui.theme.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -11,6 +13,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,32 +26,68 @@ fun PlotSurface() {
     var xFloatValue: Float by rememberSaveable { mutableStateOf(0.5f) }
     var yFloatValue: Float by rememberSaveable { mutableStateOf(0.5f) }
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Map(xPercent = xFloatValue, yPercent = yFloatValue)
-        MapSlider(
-            floatValue = xFloatValue,
-            onValueChanged = { xFloatValue = it },
-            text = stringResource(
-                id = R.string.slider_axis_value,
-                stringResource(R.string.x_label),
-                xFloatValue.toIntPercentage()
-            )
-        )
-        MapSlider(
-            floatValue = yFloatValue,
-            onValueChanged = { yFloatValue = it },
-            text = stringResource(
-                id = R.string.slider_axis_value,
-                stringResource(R.string.y_label),
-                yFloatValue.toIntPercentage()
-            )
-        )
+    val configuration = LocalConfiguration.current
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_PORTRAIT -> {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Map(xPercent = xFloatValue, yPercent = yFloatValue)
+                MapSlider(
+                    floatValue = xFloatValue,
+                    onValueChanged = { xFloatValue = it },
+                    text = stringResource(
+                        id = R.string.slider_axis_value,
+                        stringResource(R.string.x_label),
+                        xFloatValue.toIntPercentage()
+                    )
+                )
+                MapSlider(
+                    floatValue = yFloatValue,
+                    onValueChanged = { yFloatValue = it },
+                    text = stringResource(
+                        id = R.string.slider_axis_value,
+                        stringResource(R.string.y_label),
+                        yFloatValue.toIntPercentage()
+                    )
+                )
+            }
+        }
+        else -> {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Map(xPercent = xFloatValue, yPercent = yFloatValue)
+                Column {
+                    MapSlider(
+                        floatValue = xFloatValue,
+                        onValueChanged = { xFloatValue = it },
+                        text = stringResource(
+                            id = R.string.slider_axis_value,
+                            stringResource(R.string.x_label),
+                            xFloatValue.toIntPercentage()
+                        )
+                    )
+                    MapSlider(
+                        floatValue = yFloatValue,
+                        onValueChanged = { yFloatValue = it },
+                        text = stringResource(
+                            id = R.string.slider_axis_value,
+                            stringResource(R.string.y_label),
+                            yFloatValue.toIntPercentage()
+                        )
+                    )
+                }
+            }
+        }
     }
 }
 
