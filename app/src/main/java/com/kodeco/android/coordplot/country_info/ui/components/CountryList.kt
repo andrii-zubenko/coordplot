f0d0b72d6a60a287664f569b936c91a68b603695
Andrii Zubenko <andrei.zubenko@gmail.com>
2023-10-30 16:17:46 -0700

    make AnimatedStar composable with animation

--- app/src/main/java/com/kodeco/android/coordplot/country_info/ui/components/CountryList.kt
+++ app/src/main/java/com/kodeco/android/coordplot/country_info/ui/components/CountryList.kt
@@ -1,5 +1,12 @@
 package com.kodeco.android.coordplot.country_info.ui.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -7,18 +14,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Help
-import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -71,7 +82,6 @@ fun CountryList(
             items(countries.size) { index ->
                 Card(
                     modifier = Modifier
-                        .fillMaxWidth()
                         .padding(
                             horizontal = 8.dp,
                             vertical = 8.dp
@@ -79,12 +89,74 @@ fun CountryList(
                         .clickable { onCountryRowTap(index) },
                     shape = RoundedCornerShape(4.dp)
                 ) {
-                    Column(modifier = Modifier.padding(all = 8.dp)) {
-                        Text(text = "Name: ${countries[index].name.common}")
-                        Text(text = "Capital: ${countries[index].capital?.get(0).toString()}")
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Column(modifier = Modifier.padding(all = 8.dp)) {
+                            Text(text = "Name: ${countries[index].name.common}")
+                            Text(text = "Capital: ${countries[index].capital?.get(0).toString()}")
+                        }
+                        AnimatedStar()
                     }
                 }
             }
+        }
+    }
+}
+
+enum class StarState {
+    Filled, Empty
+}
+
+@Composable
+fun AnimatedStar() {
+    val starState = remember { mutableStateOf(StarState.Empty) }
+    val transition = updateTransition(targetState = starState, label = "Star Transition")
+
+    val rotation by transition.animateFloat(
+        transitionSpec = { tween(durationMillis = 500) },
+        label = "Rotation"
+    ) { starState ->
+        if (starState.value == StarState.Filled) 360f else 0f
+    }
+
+    Box(
+        modifier = Modifier
+            .clickable {
+                starState.value = when (starState.value) {
+                    StarState.Filled -> StarState.Empty
+                    StarState.Empty -> StarState.Filled
+                }
+            }
+            .padding(16.dp)
+
+    ) {
+        val imageModifier = Modifier
+            .size(24.dp)
+            .rotate(rotation)
+
+        Image(
+            painter = painterResource(
+                id = when (starState.value) {
+                    StarState.Filled -> R.drawable.star_filled
+                    StarState.Empty -> R.drawable.star_outline
+                }
+            ),
+            contentDescription = "Star Icon",
+            modifier = imageModifier
+        )
+        AnimatedVisibility(
+            visible = starState.value == StarState.Filled,
+            enter = fadeIn(animationSpec = tween(500)),
+            exit = fadeOut(animationSpec = tween(500))
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.star),
+                contentDescription = null,
+                modifier = imageModifier
+            )
         }
     }
 }
