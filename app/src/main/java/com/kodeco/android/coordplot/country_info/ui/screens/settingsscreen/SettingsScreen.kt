c836c0c1dfe18401bfc1caac659b210eb70ab17d
Andrii Zubenko <andrei.zubenko@gmail.com>
2023-11-10 15:15:24 -0800

    fill up ui for SettingsScreen

--- app/src/main/java/com/kodeco/android/coordplot/country_info/ui/screens/settingsscreen/SettingsScreen.kt
+++ app/src/main/java/com/kodeco/android/coordplot/country_info/ui/screens/settingsscreen/SettingsScreen.kt
@@ -1,9 +1,84 @@
 package com.kodeco.android.coordplot.country_info.ui.screens.settingsscreen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kodeco.android.coordplot.R
 
 @Composable
 fun SettingsScreen(onBackTap: () -> Unit) {
-    Text(text = "Settings")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.setttings_title)) },
+                navigationIcon = {
+                    IconButton(onClick = { onBackTap() }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = stringResource(R.string.back_button)
+                        )
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            FeatureToggle(toggleName = stringResource(R.string.enable_local_storage))
+            FeatureToggle(toggleName = stringResource(R.string.enable_favorites_feature))
+            FeatureToggle(toggleName = stringResource(R.string.enable_screen_rotation))
+        }
+    }
+}
+
+@Composable
+fun FeatureToggle(toggleName: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = 8.dp,
+                vertical = 8.dp
+            ),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = toggleName,
+            fontSize = 22.sp
+        )
+        Switch(
+            checked = true,
+            onCheckedChange = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SettingsScreenPreview() {
+    SettingsScreen(onBackTap = {})
 }
